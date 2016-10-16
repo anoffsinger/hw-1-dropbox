6889f7c0fba9343acd4798ac1fea9c67ce215daf
Adam Noffsinger <adam.noffsinger@uber.com>
2016-10-15 17:45:09 -0700

    First pass at entire assignment

---
+++ HW-1-Dropbox/CreateAccountFormViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateAccountFormViewController.swift
+//  HW-1-Dropbox
+//
+//  Created by Adam Noffsinger on 10/15/16.
+//  Copyright © 2016 Adam Noffsinger. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onWelcomeTap(_ sender: AnyObject) {
+        navigationController!.popViewController(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
